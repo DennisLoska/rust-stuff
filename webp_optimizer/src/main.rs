f762bd293b366977b4774509ce44d7bf95d3222e
Dennis <dennis.loska@aaron.ai>
2023-07-24 22:53:10 +0200

    🎨 fix: code review iteration

--- webp_optimizer/src/main.rs
+++ webp_optimizer/src/main.rs
@@ -2,51 +2,48 @@ use std::fs::{self, DirEntry};
 use std::path::Path;
 use std::process::Command;
 
-fn optimize_img(path: &DirEntry, path_str: &str) {
+fn optimize_img(path: &DirEntry, root_path: &Path) {
     let path_binding = path.path();
+    let root_str = root_path.to_str().expect("Path should exist.");
     let name_binding = path.file_name();
-    let path_name = path_binding.to_str().unwrap();
-    let current_data = name_binding.to_str().unwrap();
+    let path_name = path_binding.to_str().expect("Filename should exist.");
+    let current_data = name_binding.to_str().expect("Current path should exist.");
 
-    println!("{:?}", name_binding);
     let output_path = format!(
         "{}{}{}",
-        path_str,
+        root_str,
         "/optimized/",
         name_binding.to_string_lossy()
     );
 
-    let new_dir = format!("{}/{}", path_str, "optimized");
+    let new_dir = format!("{}/{}", root_str, "optimized");
     let path = Path::new(&new_dir);
 
     if !path.exists() {
         println!("Created missing folder: {:?}", path);
-        let _ = fs::create_dir_all(&path);
+        let _ = fs::create_dir_all(path);
     }
 
     if current_data.contains(".webp") {
-        let optimize = Command::new("cwebp")
-            .args(["-q", "80", path_name, "-o", output_path.as_str()])
-            .output()
-            .expect("Failed to optimize image.");
+        println!("optimizing {:?} ...", name_binding);
 
-        if optimize.status.success() {
-            let s = String::from_utf8_lossy(&optimize.stdout);
-            print!("Optimized: {}", s);
-        } else {
-            let s = String::from_utf8_lossy(&optimize.stderr);
-            print!("Error: {}", s);
+        let optimize = Command::new("cwebp")
+            .args(["-q", "80", path_name, "-o", &output_path])
+            .output()
+            .expect("Failed to optimize image");
+
+        match optimize.status.success() {
+            true => println!("Optimized {}", path_name),
+            false => println!("Error {}", String::from_utf8_lossy(&optimize.stderr)),
         }
-    } else {
-        if !path_name.contains("optimized") {
-            println!("Entering subfolder: {}", path_name);
-            image_optimizer(path_name);
-        }
+    } else if !path_name.contains("optimized") {
+        println!("Entering subfolder: {}", path_name);
+        image_optimizer(Path::new(path_name));
     }
 }
 
-fn image_optimizer(path_str: &str) {
-    let paths = fs::read_dir(path_str);
+fn image_optimizer(root_path: &Path) {
+    let paths = fs::read_dir(root_path);
     let mut path_vec: Vec<DirEntry> = Vec::new();
 
     match paths {
@@ -59,10 +56,10 @@ fn image_optimizer(path_str: &str) {
 
     path_vec
         .iter()
-        .for_each(|path| optimize_img(path, path_str));
+        .for_each(|path| optimize_img(path, root_path));
 }
 
 fn main() {
-    let root_path = "./src/images";
+    let root_path = Path::new("./src/images");
     image_optimizer(root_path);
 }
